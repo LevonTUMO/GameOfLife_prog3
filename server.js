90833e0cf964e44abbadcae1c1900c6b8acd0a3a
LevonTUMO <levon.h12.y@tumo.org>
2023-02-20 20:21:49 +0400

    Matrix from server

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
-var express = require("express");
+ matrix = [1,1,1]
+
+var express = require('express');
 
 var app = express();
 
+var server = require('http').createServer(app);
+
+var io = require('socket.io')(server);
+
+
 app.use(express.static("files"));
 
-app.get("/", function(req, res){
+app.get('/', function (req, res) {
 
-res.redirect("index.html");
-
-});
-
-app.listen(3000, function(){
-
-console.log("Example is running on port 3000");
+res.redirect('index.html');
 
 });
+
+server.listen(3000);
+
+
+io.on('connection', function (socket) {
+
+socket.emit("display message", matrix);
+
+
+  });
+
